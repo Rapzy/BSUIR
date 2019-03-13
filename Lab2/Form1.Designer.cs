4399ce16078fe1cfce84c1631b9755ff3aca4a92
rapzy <andrushko.egor@gmail.com>
2019-03-13 23:45:42 +0300

    Initial commit

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Steel Arm",
+            "Pistol",
+            "Rifle"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(171, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(687, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Create Gun";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(377, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(373, 240);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int offset = 0;
+            int margin = 40;
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                        panel1.Controls.Clear();
+                        AddField("Name", offset += margin);
+                        AddField("Type", offset += margin);
+                        break;
+                    }
+                case 1:
+                    {
+                        panel1.Controls.Clear();
+                        AddField("Name", offset += margin);
+                        AddField("Type", offset += margin);
+                        AddField("Clip Size", offset += margin);
+                        AddField("Ammo", offset += margin);
+                        break;
+                    }
+                case 2:
+                    {
+                        panel1.Controls.Clear();
+                        AddField("Name", offset += margin);
+                        AddField("Type", offset += margin);
+                        AddField("Clip Size", offset += margin);
+                        AddField("Ammo", offset += margin);
+                        AddField("Fire Rate", offset += margin);
+                        break;
+                    }
+            }
+        }
+        public void AddField(string name, int offset)
+        {
+            TextBox txt = new TextBox();
+            txt.Name = comboBox1.SelectedItem+"Box"+name.Replace(" ","");
+            txt.Height = 15;
+            txt.Width = 150;
+            txt.Top = offset;
+            panel1.Controls.Add(txt);
+
+            Label lbl = new Label();
+            lbl.Text = name;
+            lbl.Top = offset - 15;
+            panel1.Controls.Add(lbl);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
